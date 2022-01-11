699b45d09b2b0e8f135e54fbc157d9b912b94a1f
bearbaba <2200522850@qq.com>
2022-01-11 23:16:13 +0800

    uibuild

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const Note = mongoose.model('Note', noteSchema);
 Note
 	.find({})
 	.then(res=>{
-		console.log(res);
 		mongoose.connection.close();
 	})
 
@@ -58,40 +57,14 @@ const requestLogger = (req,res, next)=>{
 app.use(requestLogger);
 
 
-
-let notes = [
-		{
-			"id": 1,
-			"content": "HTML is easy",
-			"date": "2019-05-30T17:30:31.098Z",
-			"important": true
-		},
-		{
-			"id": 2,
-			"content": "Browser can execute only JavaScript",
-			"date": "2019-05-30T18:39:34.091Z",
-			"important": true
-		},
-		{
-			"id": 3,
-			"content": "GET and POST are the most important methods of HTTP protocol",
-			"date": "2019-05-30T19:20:14.298Z",
-			"important": false
-		},
-		{
-			"content": "new Note",
-			"date": "2022-01-07T08:13:20.132Z",
-			"important": false,
-			"id": 4
-		}
-	]
-
 app.get("/", (req, res) => {
 	res.send("<h1>Hello world</h1>");
 })
 
 app.get("/api/notes", (req, res) => {
-	res.send(notes);
+	Note.find({}).then(result=>{
+		res.json(result);
+	})
 })
 
 app.get("/api/notes/:id",(req,res)=>{
