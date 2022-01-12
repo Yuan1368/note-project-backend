acbd58f3ffbf9265235fbaca8f6d2c9da783a3cc
bearbaba <2200522850@qq.com>
2022-01-12 13:02:46 +0800

    uibuild

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ const noteSchema = new mongoose.Schema({
 	date: Date
 })
 
+noteSchema.set("toJSON", {
+	transform: (document, returnedObject)=>{
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	}
+
+})
+
 const Note = mongoose.model('Note', noteSchema);
 
 Note
