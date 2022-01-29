6625142db518366b98b1b800c3ae3b8b3ddb3551
bearbaba <2200522850@qq.com>
2022-01-29 23:40:42 +0800

    feat: 添加了辅助测试函数

--- test/note_api.test.js
+++ test/note_api.test.js
@@ -1,8 +1,32 @@
 const mongoose = require("mongoose")
 const supertest = require("supertest")
+const helper = require("./test_helper")
 const app = require("../app")
-
+const Note = require("../models/note")
 const api = supertest(app)
+
+beforeEach(async ()=>{
+	await Note.deleteMany({})
+	let noteObject = new Note(helper.initialNotes[0]);
+	await noteObject.save();
+	noteObject = new Note(helper.initialNotes[1]);
+	await noteObject.save();
+})
+
+test('all notes are returned', async () => {
+	const response = await api.get('/api/notes')
+
+	expect(response.body).toHaveLength(initialNotes.length)
+})
+
+test("a specific note is within the returned notes", async () => {
+	const response = await api.get("/api/notes")
+
+	const contents = response.body.map(r => r.content)
+	expect(contents).toContain(
+		'Browser can execute only Javascript'
+	)
+})
 
 test('notes are returned as json', async ()=>{
 	await api
@@ -19,6 +43,44 @@ test("there are two notes", async ()=>{
 test("the first note is about HTTP methods", async ()=>{
 	const response = await api.get("/api/notes");
 	expect(response.body[0]).toBe("new Note")
+})
+
+test("a new note is added", async ()=>{
+	const newNote = {
+		content: "a note as example",
+		important: true
+	}
+
+	await api
+		.post("/api/notes")
+		.send(newNote)
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
+
+	const notesAtEnd = await helper.notesInDb()
+	expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
+
+	const contents = notesAtEnd.map(n => n.content)
+	expect(contents).toContain(
+		'HTML is easy'
+	)
+
+})
+
+test("a new note is not added", async ()=>{
+	const newNote = {
+		// content: "a note as example",
+		important: true
+	}
+
+	await api
+		.post("/api/notes")
+		.send(newNote)
+		.expect(400)
+
+	const notesAtEnd = await helper.notesInDb()
+
+	expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
 })
 
 afterAll(() => {
